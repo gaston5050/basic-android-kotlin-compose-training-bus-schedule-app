dfd5fa7891d246f1fc67ef095fc70d86bd6f600f
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2026-06-09 17:02:00 -0300

    Segundo commit, sigo trabajando en la incializacion de la BD a partir de la preexintente

--- app/src/main/java/com/example/busschedule/data/BusScheduleDatabase.kt
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.busschedule.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [BusSchedule::class], version = 1, exportSchema = false)
@@ -10,7 +12,22 @@ abstract class BusScheduleDatabase: RoomDatabase() {
 
     companion object {
         @Volatile
-        private var INSTANCE: BusScheduleDatabase? =
+        private var INSTANCE: BusScheduleDatabase? = null
+
+        fun getDatabase(context: Context): BusScheduleDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    BusScheduleDatabase::class.java,
+                    "bus_schedule_database"
+                )
+                    .createFromAsset("database/bus_schedule.db")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
 
     }
 
